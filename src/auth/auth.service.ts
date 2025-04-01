4ae60f51d7ade69e96d8cd6588579553a332c0fa
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-04-01 14:47:20 -0500

    Post hasheo password, antes de isntalar Passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,14 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { LoginUserDto, CreateUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -17,14 +20,41 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: hashedPassword,
+      });
 
       await this.userRepository.save(user);
 
-      return user;
+      const userResponse: Partial<User> = user; // Usamos Partial para que password sea opcional
+      delete userResponse.password;
+      return userResponse;
+      // TODO: Retornar el JWT
     } catch (error: any) {
       this.handleDBErrors(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+    if (!user) throw new UnauthorizedException('Credenciales no validas');
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      throw new UnauthorizedException('Credenciales no validas');
+
+    return user;
+    //TODO: Retornar el JWT
   }
 
   private handleDBErrors(error: any): never {
